c27b8fd3cd721c5609c1d82c8e262bd2525244fe
Sergionx <sergio180203@gmail.com>
2024-03-26 12:59:22 -0400

    Refactor getPaginatedResponse function to include totalCount parameter

--- src/lib/utils/api.ts
+++ src/lib/utils/api.ts
@@ -6,28 +6,24 @@ export function paginateData<T>(data: T[], page: number, limit: number): T[] {
   return data.slice(startIndex, endIndex);
 }
 
-
 export function getPaginatedResponse<T>(
   data: T[],
+  totalCount: number,
   page: number,
-  limit: number,
+  limit: number
 ) {
-  const totalCount = data.length;
   const lastPage = Math.ceil(totalCount / limit);
 
   const nextPage = Math.min(page + 1, lastPage);
   const prevPage = Math.max(page - 1, 1);
 
-  return NextResponse.json(
-    data,
-    {
-      headers: {
-        "content-type": "application/json",
-        "X-Total-Pages": String(lastPage),
-        "X-Current-Page": String(page),
-        "X-Next-Page": String(nextPage),
-        "X-Prev-Page": String(prevPage),
-      },
-    }
-  );
+  return NextResponse.json(data, {
+    headers: {
+      "content-type": "application/json",
+      "X-Total-Pages": String(lastPage),
+      "X-Current-Page": String(page),
+      "X-Next-Page": String(nextPage),
+      "X-Prev-Page": String(prevPage),
+    },
+  });
 }
