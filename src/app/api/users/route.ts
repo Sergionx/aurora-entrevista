e46509623c2ab101fb28a56a3e843b8b8ee4233c
Sergionx <sergio180203@gmail.com>
2024-03-26 12:49:44 -0400

    Refactor GET function in users route to handle pagination and limit validation

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,28 +1,29 @@
 import { UserData } from "@/lib/interfaces/UserData";
 import { getPaginatedResponse, paginateData } from "@/lib/utils/api";
 import { readCSVFile } from "@/lib/utils/csv";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
-export async function GET(request: Request) {
-  const urlParams = new URLSearchParams(request.url);
+export async function GET(request: NextRequest) {
+  const urlParams = request.nextUrl.searchParams;
   const page = Number(urlParams.get("page")) || 1;
   const limit = Number(urlParams.get("limit")) || 10;
 
-  if (page < 1 || limit < 1) {
+  if (page < 1 || limit < 1)
     return new NextResponse("Invalid page or limit", { status: 400 });
-  }
 
-  if (limit > 100) {
+  if (limit % 10 !== 0)
+    return new NextResponse("Limit must be a multiple of 10", { status: 400 });
+
+  if (limit > 100)
     return new NextResponse("Limit cannot be greater than 100", {
       status: 400,
     });
-  }
 
   const results = await readCSVFile<UserData>("src/app/api/data/users.csv");
 
   const paginatedData = paginateData(results, page, limit);
 
-  return getPaginatedResponse(paginatedData, page, limit);
+  return getPaginatedResponse(paginatedData, results.length, page, limit);
 }
