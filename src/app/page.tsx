26f29cf4978be6090265e5b669ca9efcb14bc37d
Sergionx <sergio180203@gmail.com>
2024-03-26 13:00:05 -0400

    Refactor Home component to include pagination and search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
 import { getUsers } from "@/lib/api";
 import { DataTable } from "@/lib/components/table/DataTable";
 import { userColumns } from "./userColumn";
+import { getNumberFromString } from "@/lib/utils/searchParams";
 
-export default async function Home() {
-  const users = await getUsers();
-  // console.log(users);
+export default async function Home({
+  searchParams: { page = "1", limit = "10" },
+}: {
+  searchParams: {
+    page: string;
+    limit: string;
+  };
+}) {
+  const pageNumber = getNumberFromString(page);
+  const limitNumber = getNumberFromString(limit);
 
+  const { data: users, pagination } = await getUsers(pageNumber, limitNumber);
+
+  // TODO - Escojer color bonito de fondo
   return (
-    <main>
-      <DataTable
-        data={users}
-        columns={userColumns}
-        filterOptions={{
-          field: "email",
-          placeholder: "Search by email",
-        }}
-      />
-    </main>
+    <DataTable
+      data={users}
+      columns={userColumns}
+      addUrl="/users/add"
+      filterOptions={{
+        field: "email",
+        placeholder: "Search by email",
+      }}
+      paginationData={pagination}
+    />
   );
 }
