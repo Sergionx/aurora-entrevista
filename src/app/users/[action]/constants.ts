035c26197fff439f221542198a23354cfbe393ec
Sergionx <sergio180203@gmail.com>
2024-03-26 15:48:11 -0400

    Add form validation schema for user data

--- src/app/users/[action]/constants.ts
+++ src/app/users/[action]/constants.ts
@@ -1,1 +1,13 @@
+import { z } from "zod";
+
 export type FormMode = "add" | "edit" | "view";
+
+export const formSchema = z.object({
+  firstName: z.string().min(5).max(50),
+  lastName: z.string().min(5).max(50),
+  email: z.string().email({
+    message: "El email debe ser un email válido",
+  }),
+  moneySpent: z.number().positive(),
+  productsPurchased: z.number().positive(),
+});
