cf5b269518fd524e276e2710f6f938580a2552bb
Sergionx <sergio180203@gmail.com>
2024-03-25 21:45:22 -0400

    Add UserData interface

---
+++ src/lib/interfaces/UserData.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  moneySpent: number;
+  productsPurchased: number;
+  updatedAt: Date;
+  createdAt: Date;
+}
