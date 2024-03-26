b38ebe95427bc6a196e80f199c67db9e5aa8cc0f
Sergionx <sergio180203@gmail.com>
2024-03-26 15:45:21 -0400

    Add user route for GET and PATCH methods

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,46 @@
+import { UserCSVData } from "@/lib/interfaces/UserData";
+import { readCSVFile, writeCSVFile } from "@/lib/utils/csv";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic"; // defaults to auto
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const users = await readCSVFile<UserCSVData>("src/app/api/data/users.csv");
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return new NextResponse("User Id does not exists", { status: 404 });
+  }
+
+  return NextResponse.json(user);
+}
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const users = await readCSVFile<UserCSVData>("src/app/api/data/users.csv");
+
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex === -1) {
+    return new NextResponse("User Id does not exists", { status: 404 });
+  }
+
+  const body = await request.json();
+
+  users[userIndex] = {
+    ...users[userIndex],
+    ...body,
+  };
+
+  await writeCSVFile<UserCSVData>("src/app/api/data/users.csv", users);
+
+  return new NextResponse("User updated", { status: 200 });
+}
