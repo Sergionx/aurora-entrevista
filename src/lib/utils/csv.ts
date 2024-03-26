b323f9c165710008c5d9d220255ad7183e8a96c0
Sergionx <sergio180203@gmail.com>
2024-03-26 19:07:33 -0400

    Changed csv library for easier writing

--- src/lib/utils/csv.ts
+++ src/lib/utils/csv.ts
@@ -1,22 +1,35 @@
 import fs from "fs";
-import csv from "csv-parser";
+import { parse, stringify } from "csv";
+import { UserCSVData } from "../interfaces/UserData";
 
-export async function readCSVFile<T>(path: string) {
+export async function readCSVFile<T>(
+  path: string,
+  onData: (data: string[]) => T
+) {
   const results: T[] = [];
   return new Promise<T[]>((resolve, reject) => {
     fs.createReadStream(path)
-      .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => {
-        resolve(results);
+      .pipe(parse({ delimiter: ",", from_line: 2 }))
+      .on("data", (data) => {
+        const dataRow = onData(data);
+
+        results.push(dataRow);
       })
+      .on("end", () => resolve(results))
       .on("error", (error) => reject(error));
   });
 }
 
-export async function writeCSVFile<T>(path: string, data: T[]) {
-  return new Promise<void>((resolve, reject) => {
-    const writeStream = fs.createWriteStream(path);
-    writeStream.write(data.join("\n"));
+export function writeCSVFile<T extends Object>(path: string, data: T[]) {
+  const writableStream = fs.createWriteStream(path);
+
+  const columns = [...Object.keys(data[0])];
+
+  const stringifier = stringify({ header: true, columns: columns });
+
+  data.forEach((row) => {
+    stringifier.write(row);
   });
+
+  stringifier.pipe(writableStream);
 }
