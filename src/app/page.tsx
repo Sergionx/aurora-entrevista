13438137c133f07d458f9fd41054e1a11eff7b09
Sergionx <sergio180203@gmail.com>
2024-03-26 22:33:33 -0400

    Add AlertDialog component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,16 @@ import { getUsers } from "@/lib/api";
 import { DataTable } from "@/lib/components/table/DataTable";
 import { userColumns } from "./userColumn";
 import { getNumberFromString } from "@/lib/utils/searchParams";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/lib/components/shadcn/alert-dialog";
 
 export default async function Home({
   searchParams: { page = "1", limit = "10" },
@@ -18,15 +28,33 @@ export default async function Home({
 
   // TODO - Escojer color bonito de fondo
   return (
-    <DataTable
-      data={users}
-      columns={userColumns}
-      addUrl="/users/add"
-      filterOptions={{
-        field: "email",
-        placeholder: "Search by email",
-      }}
-      paginationData={pagination}
-    />
+    <AlertDialog>
+      <DataTable
+        data={users}
+        columns={userColumns}
+        addUrl="/users/add"
+        filterOptions={{
+          field: "email",
+          placeholder: "Search by email",
+        }}
+        paginationData={pagination}
+      />
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>¿Está totalmente seguro?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta acción no se puede deshacer. Esta acción eliminará
+            permanentemente los datos del usuario.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction className="bg-error text-white">
+            Eliminar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
