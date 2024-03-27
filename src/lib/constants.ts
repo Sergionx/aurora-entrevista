a65917401d67152521ebdb7e7521a49f32ab1ef3
Sergionx <sergio180203@gmail.com>
2024-03-26 23:11:09 -0400

    Add console.log statement to print baseUrl

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,5 @@ export const baseUrl =
   process.env.NODE_ENV === "production"
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
+    console.log(baseUrl)
