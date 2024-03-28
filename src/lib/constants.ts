5eed7528e175213c7016222c7de9dae1cafdf219
Sergionx <sergio180203@gmail.com>
2024-03-28 19:58:56 -0400

    Update baseUrl in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,1 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ?? "";
