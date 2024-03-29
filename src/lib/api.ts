5d22a27e39970c24ef5ab700a41d978eb409935e
Sergionx <sergio180203@gmail.com>
2024-03-28 20:01:49 -0400

    Add console log to display API URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -14,6 +14,7 @@ export async function getUsers(page = 1, limit = 10) {
   });
 
   console.log("antes del fetch");
+  console.log(`${baseUrl}/api/users?${params}`)
   const request = await fetch(`${baseUrl}/api/users?${params}`, {
     next: {
       tags: ["users"],
